c93dbd099d07439358e1d97ae53661d5ae75a46c
nn-firc <53158768+nn-firc@users.noreply.github.com>
2022-11-05 16:09:24 +0800

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -4,5 +4,5 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-./waf configure -T release --android=armeabi-v7a-hard,4.9,21 --disable-warns &&
+./waf configure -T release --android=armeabi-v7a-hard,4.9,21 &&
 ./waf build -j2
